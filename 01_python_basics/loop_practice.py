9d2eb664eb2ec73b3a46edbc773d5f8eb27726d5
Elavarasi.R <162303324+Elavarasi2@users.noreply.github.com>
2026-07-15 15:19:27 +0530

    Add number reversal and palindrome check

--- 01_python_basics/loop_practice.py
+++ 01_python_basics/loop_practice.py
@@ -1,1 +1,23 @@
+# Reverse a number 
+num = 123
+rev   = 0
+while num > 0:
+    rem = num % 10
+    rev = rev * 10 + rem
+    num //= 10
+print(rev)
 
+
+# Palindrome
+num = 101
+temp = num
+rev   = 0
+while num > 0:
+    rem = num % 10
+    rev = rev * 10 + rem
+    num //= 10
+print(rev) # for checking
+if temp == rev :
+    print("This is a palindrome")
+else :
+    print("Not a palindrome")
