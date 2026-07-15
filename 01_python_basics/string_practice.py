5352f53f126cdb6b1b52228aff1a188f3109c6e1
Elavarasi.R <162303324+Elavarasi2@users.noreply.github.com>
2026-07-15 15:21:04 +0530

    String implementations

---
+++ 01_python_basics/string_practice.py
@@ -0,0 +1,10 @@
+# If I want to quote the word in f strings can use backslash 
+import random
+list = ["Ela", "Sheero", "Liya","Teesa"]
+lucky_charm = random.choice(list)
+print(f"The person who sings next is \"{lucky_charm}\"")
+
+
+print("Data engineering is the software engineering" 
+" discipline focused on designing, building, and maintaining" 
+" the systems and pipelines that collect, store, and transform raw data at")
