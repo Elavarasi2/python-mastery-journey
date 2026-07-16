63c990e6b4bc03dc21e784aa39d7bb2d1e3e2f05
Elavarasi.R <162303324+Elavarasi2@users.noreply.github.com>
2026-07-16 14:46:34 +0530

    Implement string reversal
    Add a script to reverse a string using for and while loops.

---
+++ 01_python_basics/Reversing_String.py
@@ -0,0 +1,14 @@
+# Using for Loop
+word = "Python"
+l = len(word)
+for i in range(l,0,-1):
+    print(word[i-1],end="")
+
+# Using While Loop
+word = "Elavarasi"
+l = len(word)
+while l > 0:
+    
+    print(word[l-1],end="")
+    l-=1
+  
