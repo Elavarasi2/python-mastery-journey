87c6063994e0a72f6017773c23ab3e9f6439283e
Elavarasi.R <162303324+Elavarasi2@users.noreply.github.com>
2026-07-24 10:57:10 +0530

    Implement vowel counting in user input
    Added a program to count vowels in a user's name.

--- 01_python_basics/Iterable_practice.py
+++ 01_python_basics/Iterable_practice.py
@@ -1,1 +1,9 @@
 
+
+# String Iterable
+word = input("Enter your name : ")
+count = 0
+for letter in word:
+    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U":
+        count+=1
+print(f"The vowel count in this name is {count}")
