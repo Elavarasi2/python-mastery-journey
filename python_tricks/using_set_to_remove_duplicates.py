ba8ad46aef44c22f77052f9a67f178d4c6a5ce2a
Elavarasi.R <162303324+Elavarasi2@users.noreply.github.com>
2026-07-27 15:36:56 +0530

    Add example of using set to remove duplicates
    Demonstrate using a set to remove duplicate characters from a string.

---
+++ python_tricks/using_set_to_remove_duplicates.py
@@ -0,0 +1,11 @@
+string = "banana"
+print(" ".join(set(string))(
+print(set(string))
+
+# A set has two important properties:
+
+#  --It stores unique elements only.
+#  --It does not preserve the order of elements.
+# A set uses a special data structure called a hash table.
+# It places elements based on their hash value, not on the order they were added.
+# So the printed order is based on internal storage, not insertion order.
