7ebddc1c071563bb5736a189b1542dbb78c56fd9
Elavarasi.R <162303324+Elavarasi2@users.noreply.github.com>
2026-09-02 12:27:38 +0530

    Implement calculate function for sum and product
    Created a function to calculate sum and product.

---
+++ Revision_Pratice_Challenges/revision_01_python_fundamentals/07_functions_return.py
@@ -0,0 +1,10 @@
+# Functions & Return Practice
+#
+# TODO:
+# Revisit this challenge after studying the Functions phase.
+#
+# Requirement:
+# Create a function calculate(a, b)
+# that RETURNS the sum and product.
+#
+# Then use those returned values outside the function.
